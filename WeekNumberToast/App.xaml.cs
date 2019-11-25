5582fa1de1d65aa727adc3bffcc14d7ac97d85cb
Tom Richter <trichter@live.com>
2019-11-25 11:33:41 -0500

    Basic Windowless NotifyIcon showing using wpf-notifyicon package

---
+++ WeekNumberToast/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace WeekNumberToast
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
+    }
+}
