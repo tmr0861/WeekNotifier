4664a6e64e96344c41214bfbd5ab0d29cbe70d45
Tom Richter <trichter@live.com>
2020-01-02 18:09:41 -0500

    Moved to framework .csproj to get OneClick working.

---
+++ WeekNotifier.org/Helpers/DelegateCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WeekNotifier.Helpers
+{
+    /// <summary>
+    /// Simplistic delegate command.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// Gets or sets the command action.
+        /// </summary>
+        /// <value>The command action.</value>
+        public Action CommandAction { get; set; }
+
+        /// <summary>
+        /// Gets or sets the can execute function.
+        /// </summary>
+        /// <value>The can execute function.</value>
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
+        /// <returns><see langword="true" /> if this command can be executed; otherwise, <see langword="false" />.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null  || CanExecuteFunc();
+        }
+
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
